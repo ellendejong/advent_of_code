34a9ad1f5e3c83291a2a8f4fa93ed6345979e48e
ellendejong <de.jong.ellen@hotmail.com>
2024-12-02 13:36:36 +0100

    Puzzle1: calculate distance, included pytests.

---
+++ advent_of_code/aoc_2024/get_distance.py
@@ -0,0 +1,102 @@
+import argparse
+import pandas
+from pathlib import Path
+from sys import argv
+
+def validate_non_empty_existing_path(file_or_dir):
+    """
+    This function checks whether the provided file or dir exists and is not empty.
+
+    Args:
+        file_or_dir (string): Input file or directory
+
+    Raises:
+        FileNotFoundError: If input string file_or_dir is neither a file nor a dir.
+        OSError: If input is not a dir and file is empty.
+
+    Returns:
+        string: Provided input file or directory. If dir, suffix '/' might be added.
+    """
+    input_path = Path(file_or_dir)
+    if not input_path.is_file() and not input_path.is_dir():
+        raise FileNotFoundError(errno_ENOENT, os_strerror(errno_ENOENT), file_or_dir)
+    elif not input_path.is_dir() and not input_path.stat().st_size:
+        raise OSError(f"File {file_or_dir} is empty.")
+    elif input_path.is_dir() and file_or_dir[::-1][0] != '/':
+        return f"{file_or_dir}/"
+    else:
+        return file_or_dir
+
+
+def parse_arguments_and_check(args_in):
+    """
+    Parses arguments and validates / checks format of input.
+
+    Args:
+        args_in (list of strings): Commandline input arguments.
+
+    Returns:
+        Namespace: Convert argument strings to objects and assign them as attributes of the namespace.
+    """
+    parser = argparse.ArgumentParser(description='Check kinship output based on ped file.')
+    parser.add_argument('input_file', type=validate_non_empty_existing_path, help='Input file with numbers.')
+    arguments = parser.parse_args(args_in)
+    return arguments
+
+
+def read_data_and_get_columns_as_list(in_file, sep='\t'):
+    """
+    Read input data and parse columns as sorted lists.
+
+    Args:
+        in_file (str): Path to input .txt file
+        sep (str, optional): Separator used in input file. Defaults to '\t'.
+
+    Returns:
+        tuple of two lists: columns 'left' and 'right' as sorted lists.
+    """
+    df_numbers = pandas.read_csv(
+        in_file,
+        quotechar="'",
+        sep=sep,
+        names=["left", "right"],
+        skip_blank_lines=True,
+        dtype=int
+    )
+    lst_left_sorted = df_numbers.left.sort_values(ascending=True).tolist()
+    lst_right_sorted = df_numbers.right.sort_values(ascending=True).tolist()
+    return lst_left_sorted, lst_right_sorted
+
+
+def get_distance_two_lists(lst_left, lst_right):
+    """
+    Calculate distance between integers in list (by order)
+
+    Args:
+        lst_left (list): list with integers
+        lst_right (list): list with integers
+
+    Raises:
+        ValueError: When input lists are of unequal lengths.
+
+    Returns:
+        int: Sum of retrieved distances of pairwise comparison in list.
+    """
+    if len(lst_left) != len(lst_right):
+        raise ValueError(f"Unequal length of lists, {len(lst_left)} vs {len(lst_right)}")
+    distances = [abs(int_left - int_right) for int_left, int_right in zip(sorted(lst_left), sorted(lst_right))]
+    return sum(distances)
+
+
+def print_summed_distance(input):
+    lst_left, lst_right = read_data_and_get_columns_as_list(input)
+    summed_distance = get_distance_two_lists(lst_left, lst_right)
+    print(f"The summed distance is: {summed_distance}")
+
+
+def main():
+    arguments = parse_arguments_and_check(args_in=argv[1:])
+    print_summed_distance(input=arguments.input_file)
+
+if __name__ == '__main__':
+    main()
---
+++ advent_of_code/aoc_2024/tests/test_get_distance.py
@@ -0,0 +1,60 @@
+import pytest
+
+from advent_of_code.aoc_2024 import get_distance
+
+
+class TestReadDataAndGetColumnsAsList():
+    @pytest.mark.parametrize("filename,exp_length", [
+        ("single_pair.txt", 1),
+        ("multi_pair.txt", 2),
+        ("values_int_str.txt", 1),
+    ])
+    def test_input_ok(self, filename, exp_length, datadir):
+        left, right = get_distance.read_data_and_get_columns_as_list(f"{datadir}/{filename}")
+        assert len(left) == exp_length
+        assert len(right) == exp_length
+
+    @pytest.mark.parametrize("filename,exp_col_index", [
+        ("missing_value_left.txt", 0),
+        ("missing_value_right.txt", 1),
+    ])
+    def test_input_error(self, filename, exp_col_index, datadir):
+        with pytest.raises(ValueError) as value_error:
+            get_distance.read_data_and_get_columns_as_list(f"{datadir}/{filename}")
+        assert f"Integer column has NA values in column {exp_col_index}" == str(value_error.value)
+
+
+class TestGetDistanceTwoLists():
+    @pytest.mark.parametrize("left,right,exp_sum_distance", [
+        # Test single value, no distance
+        ([1], [1], 0),
+        # Test single value, positive distance
+        ([1], [2], 1),
+        # Test single value, negative distance
+        ([1], [-1], 2),
+        # Test multi value, no distance, sort irrelevant.
+        ([1, 1], [1, 1], 0),
+        # Test multi value, positive distance, sort irrelevant.
+        ([1, 1], [2, 2], 2),
+        # Test multi value, negative distance, sort irrelevant.
+        ([1, 1], [-1, -1], 4),
+        # Test multi value, no distance, sort relevant.
+        ([1, 2], [2, 1], 0),
+        # Test multi value, positive distance, sort relevant.
+        ([1, 2], [3, 2], 2),
+        # Test multi value, negative distance, sort relevant.
+        ([2, 1], [-2, -1], 6)
+    ])
+    def test_get_distance_two_lists(self, left, right, exp_sum_distance):
+        result = get_distance.get_distance_two_lists(lst_left=left, lst_right=right)
+        assert result == exp_sum_distance
+
+    @pytest.mark.parametrize("left,right", [
+        # Test unequal list lengths
+        ([1, 1], [2]),
+        ([1], [2, 1])
+    ])
+    def test_error_input(self, left, right):
+        with pytest.raises(ValueError) as value_error:
+            get_distance.get_distance_two_lists(lst_left=left, lst_right=right)
+        assert f"Unequal length of lists, {len(left)} vs {len(right)}" == str(value_error.value)
